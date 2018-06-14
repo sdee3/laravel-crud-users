7d39d0d2031079cab9e58b14db65cc9ac1d19608
Stefan <stefandjokic@wegrowth.com>
2018-06-14 09:27:03 +0200

    Removed excess font

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -12,9 +12,6 @@
 
         <title>Laravel Users CRUD</title>
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Lato:100,600" rel="stylesheet" type="text/css">
-
     </head>
     <body>
         <div id="app">
